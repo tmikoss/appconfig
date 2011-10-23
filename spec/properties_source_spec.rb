d9b002b29f2d3623fe04818dac037b1c228751b7
Toms Mikoss <toms.mikoss@gmail.com>
2011-10-23 10:30:22 +0300

    Replaced not yet implemented features for .properties files with pending tests, updated readme.

--- spec/properties_source_spec.rb
+++ spec/properties_source_spec.rb
@@ -14,16 +14,6 @@ describe "AppCfg retrieval from Properties" do
       AppCfg.app_name.should == 'AppCfg'
     end
 
-    # Currently N/A to .properties files
-    #
-    # it "should allow multi-level access with hash syntax" do
-    #   AppCfg['admin_credentials']['username'].should == 'admin'
-    # end
-    #
-    # it "should allow multi-level access with object syntax" do
-    #   AppCfg.admin_credentials.username.should == 'admin'
-    # end
-
     it "should return nil on non-existent key, hash syntax" do
       AppCfg['wrong_key'].should be_nil
     end
@@ -38,26 +28,37 @@ describe "AppCfg retrieval from Properties" do
       AppCfg['has'].should == "five trailing spaces     "
     end
 
-    # Not yet supported
-    # it "should support colon-delimited properties" do
-    #   AppCfg['colon'].should == "another delimiter"
-    # end
-    #
-    # it "should trim spaces around delimiter" do
-    #   AppCfg["website"].should == "http://en.wikipedia.org/"
-    # end
-    #
-    # it "should support multi-line values" do
-    #   AppCfg["message"].should == "Welcome to         Wikipedia!"
-    # end
-    #
-    # it "should support keys with spaces" do
-    #   AppCfg["key with spaces"].should == 'This is the value that could be looked up with the key "key with spaces".'
-    # end
-    #
-    # it "should support Unicode escapes" do
-    #   AppCfg["tab"].should == "\t"
-    # end
+    context "TODO" do
+      before { pending "not yet supported" }
+      
+      it "should allow multi-level access with hash syntax" do
+        AppCfg['admin_credentials']['username'].should == 'admin'
+      end
+      
+      it "should allow multi-level access with object syntax" do
+        AppCfg.admin_credentials.username.should == 'admin'
+      end
+      
+      it "should support colon-delimited properties" do
+        AppCfg['colon'].should == "another delimiter"
+      end
+      
+      it "should trim spaces around delimiter" do
+        AppCfg["website"].should == "http://en.wikipedia.org/"
+      end
+      
+      it "should support multi-line values" do
+        AppCfg["message"].should == "Welcome to         Wikipedia!"
+      end
+      
+      it "should support keys with spaces" do
+        AppCfg["key with spaces"].should == 'This is the value that could be looked up with the key "key with spaces".'
+      end
+      
+      it "should support Unicode escapes" do
+        AppCfg["tab"].should == "\t"
+      end
+    end
   end
 end
 
