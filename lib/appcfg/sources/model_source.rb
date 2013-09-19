afea403d5c0734b8dd8d7892e0f66dc388acdd02
Krzysiek Szczuka <kszczuka@gmail.com>
2013-09-19 08:48:39 +0200

    model_source as a tree

--- lib/appcfg/sources/model_source.rb
+++ lib/appcfg/sources/model_source.rb
@@ -3,11 +3,22 @@ module AppCfg
     def initialize(options = {})
       @model_class = options[:class]
     end
-    
+
     def reload_data!
       @hash = {}
       @model_class.all.each do |instance|
-        @hash[instance.key] = instance.value
+        #@hash[instance.key] = instance.value
+        hsh = @hash
+        idx = {}
+        arr = instance.key.split('.')
+        arr.each_with_index do |x,i|
+          hsh[x] ||= {}
+          if arr.size-1 == i
+            hsh[x] = instance.value
+          else
+            hsh = hsh[x]
+          end
+        end
       end
     end
   end
